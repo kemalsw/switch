b21c52dce08b89edefe2718b479a8dea22cc8250
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-17 15:57:30 +0300

    First commit

--- ConsoleApp28/Program.cs
+++ ConsoleApp28/Program.cs
@@ -10,27 +10,35 @@ namespace ConsoleApp28
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ATM");
-            Console.WriteLine("Lütfen bir seçenek seçiniz: ");
-            Console.WriteLine("1 para çekme");
-            Console.WriteLine("2 para yatırma");
-            Console.WriteLine("3 Bakiye sorgulama");
-            Console.WriteLine("4 Çıkış yap");
-            int a = Convert.ToInt32(Console.ReadLine());
-            switch (a)
+            /*Console.WriteLine("Ekrandan girilen sayıya kadar tek sayıları ekrana yazdır");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=sayi; i++)
             {
-                case 1: Console.WriteLine("Para çekebilirsiniz");
-                    break;
-                case 2: Console.WriteLine("Para yatırabilirsiniz");
-                    break;
-                case 3: Console.WriteLine("Bakiyenizi görebilirsiniz");
-                    break;
-                case 4: Console.WriteLine("Çıkış yaptınız");
-                    break;
-                default:
-                    Console.WriteLine("Lütfen geçerli bir seçeneği tuşlayınız");
-                    break;
+                if (i%2==1)
+                {
+                    Console.WriteLine(i);
+                }
             }
+            Console.ReadLine();*/
+
+
+            
+            int tektoplam = 0;
+            int cifttoplam = 0;
+            Console.WriteLine("1 ile 1000 arasındaki tek ve çift sayıların toplamlarını ekrana göster");
+            for (int i = 1; i <=1000; i++)
+            {
+                if (i%2==0)
+                {
+                    cifttoplam += i;
+                }
+                else if (i%2==1)
+                {
+                    tektoplam += i;
+                }
+            }
+            Console.WriteLine("Tek sayıların toplamı: "+tektoplam);
+            Console.WriteLine("Çift sayıların toplamı: "+cifttoplam);
             Console.ReadLine();
         }
     }
